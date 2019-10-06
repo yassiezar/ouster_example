b8a20466ce7303c9023dc4f23054fb9e643947ab
Wil Selby <accounts@wilselby.com>
2019-10-06 09:28:17 -0700

    updating reference commands

--- run-docker.sh
+++ run-docker.sh
@@ -40,27 +40,27 @@ fi
 #rosrun cartographer_ros cartographer_rosbag_validate -bag_filename /root/bags/office_demo_9_25_19.bag
 
 ## Run 2D online
-#roslaunch demo_cart_2d.launch bag_filename:=/root/bags/office_demo_9_25_19.bag
+# roslaunch demo_cart_2d.launch bag_filename:=/root/bags/office_demo_9_25_19.bag
 
 ## Run 2D offline (to generate .pbstream file)
-#roslaunch offline_cart_2d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag
+# roslaunch offline_cart_2d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag
 
 ## Run 2D assets_writer
-#roslaunch assets_writer_cart_2d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag  pose_graph_filename:=/root/bags/office_demo_9_25_19.bag.pbstream 
+# roslaunch assets_writer_cart_2d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag  pose_graph_filename:=/root/bags/office_demo_9_25_19.bag.pbstream 
 
 ## View output pngs
-#xdg-open office_demo_9_25_19.bag_xray_xy_all.png
-#xdg-open office_demo_9_25_19.bag_probability_grid.png
+# xdg-open office_demo_9_25_19.bag_xray_xy_all.png
+# xdg-open office_demo_9_25_19.bag_probability_grid.png
 
 ## Run 3d online
-#roslaunch demo_cart_3d.launch bag_filename:=/root/bags/office_demo_9_25_19.bag
+# roslaunch demo_cart_3d.launch bag_filename:=/root/bags/office_demo_9_25_19.bag
 
 ## Run 3d offline
-#roslaunch offline_cart_3d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag 
+# roslaunch offline_cart_3d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag 
 
 ## Run 3d assets_writer
-#roslaunch assets_writer_cart_3d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag  pose_graph_filename:=/root/bags/office_demo_9_25_19.bag.pbstream 
+# roslaunch assets_writer_cart_3d.launch bag_filenames:=/root/bags/office_demo_9_25_19.bag  pose_graph_filename:=/root/bags/office_demo_9_25_19.bag.pbstream 
 
-#xdg-open office_demo_9_25_19.bag_xray_xy_all.png
-#xdg-open office_demo_9_25_19.bag_probability_grid.png
+# xdg-open office_demo_9_25_19.bag_xray_xy_all.png
+# xdg-open office_demo_9_25_19.bag_probability_grid.png
 
